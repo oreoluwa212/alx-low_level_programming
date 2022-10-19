071b5fbbb3b9d08133b45d1dd411766698db1281
oreoluwa212 <oreoluwajayyiruth@gmail.com>
2022-10-19 04:52:35 -0700

    again

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,6 +11,6 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
-int print_to_98(int);
-int _putchar(int c);
+void print_to_98(int n);
+int _putchar(char c);
 #endif
